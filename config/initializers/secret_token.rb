ed34b57acc80ccae7d503d8a1171588189b4be0a
brewtech <evan@brewtech.us>
2013-11-12 16:10:59 -0500

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ist420::Application.config.secret_token = '7e68098bf738c660af11872c9b4ca91c58399791d7edaa7cafb04c7fab94b2e59cdae1a4febe31588687ad74b764a236296717c61fd49b639242091a30640d3a'
